36ae9725a47ef8a6f5088012676cdbc7879b92ad
pkk22 <iec2024070@iiita.ac.in>
2025-12-26 11:46:06 +0000

    datatypesSummary

---
+++ 01_basics/datatypesSummary.js
@@ -0,0 +1,53 @@
+// # Primitive data types (passed by value)
+
+// 7 types
+// String , Number , Boolean , null (means empty e.g. tempratur=null means that no temprature is alloted)
+// undefined , symbol(used to make any value unique) 
+// BigInt
+
+let score = 100
+let scoreValue = score
+scoreValue = 200;
+
+console.log(score)
+console.log(scoreValue);
+
+
+// even if we pass same string or number in symbol it will make two unqiue things
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// id and anotherId are not equal
+console.log(id===anotherId)
+
+
+const outsideTemperature = null
+
+const bigNumber = 2453876456267562628n;
+
+// # Reference type (passed by reference)
+
+// Array , object , functions
+
+const bday = [6,21,22,23] //array
+
+let myObj={
+    name: "Priya",
+    age: 17,
+}
+
+const myFunction= function(){
+    console.log("Hello World")
+}
+
+// for knowing data type of any variable or const
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemperature); //null is an object
+
+let Hello
+console.log(Hello);
+console.log(typeof Hello);
+
+// type of all non primitive datatype is object
+
+
